3f55ee1a7049df6f2aaf5d02fd8261a5ffccbe37
billchungiii <billchungiii@gmail.com>
2018-05-02 03:03:57 +0800

    加入專案檔案。

---
+++ OrderZodiacsSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderZodiacsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list
+               = new List<string> { "牛", "豬", "鼠", "羊", "狗" };
+
+            foreach (var item in list.OrderBy((x) => x, new ZCompare()))
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+
+        public class ZCompare : IComparer<string>
+        {
+            private static List<string> zodiacs = new List<string>()
+            {
+                "鼠", "牛", "虎", "兔", "龍" , "蛇", "馬", "羊" , "猴", "雞", "狗", "豬"
+            };
+            public int Compare(string x, string y)
+            {
+                return (zodiacs.IndexOf(x) - zodiacs.IndexOf(y));
+            }
+        }
+    }
+}
